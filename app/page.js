27359c0ac761fbfb3db200ac0dbbb7f172af16ee
AhrensOG <ahrensgabriel09@gmail.com>
2024-01-27 13:10:58 -0300

    Add Hero component and their images

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client'
 
 import Navbar from "@/components/Navbar/Navbar";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main className="">
-      <Navbar/> 
+      <Navbar/>
+      <Hero/> 
     </main>
   );
 }
