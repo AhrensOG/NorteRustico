528270fbaa043bcdc712b51a4fd563e996cce525
AhrensOG <ahrensgabriel09@gmail.com>
2024-01-27 22:30:17 -0300

    Add new global styles | Add Footer Logo and their component | Add Navbar and Footer to project base

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
-'use client'
+"use client";
 
-import Navbar from "@/components/Navbar/Navbar";
 import Hero from "@/components/home/hero/Hero";
 import ProductsSection from "@/components/home/products/ProductsSection";
 
 export default function Home() {
   return (
-    <main className="">
-      <Navbar/>
-      <Hero/> 
-      <ProductsSection/>
+    <main>
+      <Hero />
+      <ProductsSection />
     </main>
   );
 }
