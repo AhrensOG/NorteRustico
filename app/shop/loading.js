8a3537f5f51a5d6bd1945b6cb4bdd6aba3bfbc6f
AhrensOG <ahrensgabriel09@gmail.com>
2024-01-29 15:21:29 -0300

    Add Filters to Shop Page | Fix typeError in loading component

--- app/shop/loading.js
+++ app/shop/loading.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div class="flex items-center justify-center w-full h-full">
-      <div class="border-t-2 border-[#523900] rounded-full animate-spin h-12 w-12"></div>
+    <div className="flex items-center justify-center w-full h-full">
+      <div className="border-t-2 border-[#523900] rounded-full animate-spin h-12 w-12"></div>
     </div>
   );
 };
