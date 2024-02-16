f72c5c9f6e20491ce587225bf912e1e05d2ce976
AhrensOG <ahrensgabriel09@gmail.com>
2024-02-16 18:50:20 -0300

    Add favourites functionality to Favourite page | Add Favourite function to Card's components | Add favourite's actions and endpoints

--- components/shop/auxiliarComponents/ProductDetailCarousel.jsx
+++ components/shop/auxiliarComponents/ProductDetailCarousel.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import Favourites from "../../product/auxiliarComponents/Favourites";
 
 const ProductDetailCarousel = ({
   images,
@@ -54,7 +55,7 @@ const ProductDetailCarousel = ({
                   className="object-cover object-center rounded-xl transition-opacity opacity-0 duration-500 p-0.5"
                   onLoad={(event) => event.target.classList.remove("opacity-0")}
                   sizes="(max-width: 150px) 15vw, 15vw"
-                  loading="lazy"
+                  priority
                   quality={100}
                 />
               </div>
@@ -111,30 +112,12 @@ const ProductDetailCarousel = ({
             alt={`Slide ${currentIndex + 1}`}
             onLoad={(event) => event.target.classList.remove("opacity-0")}
             sizes="(max-width: 480px) 100vw, 480px"
-            loading="lazy"
+            priority
             quality={100}
           />
         </div>
         {favourites && (
-          <div className="w-full flex flex-row justify-center items-center py-2">
-            <span className="text-[#C9140F] flex flex-row text-lg items-center gap-2 cursor-pointer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 stroke-[#C9140F]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                />
-              </svg>
-              Agregar a Favoritos
-            </span>
-          </div>
+          <Favourites />
         )}
       </div>
     </div>
