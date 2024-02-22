030914e2c7f6e9328ec0aad69abb58a5f4af478f
AhrensOG <ahrensgabriel09@gmail.com>
2024-02-22 16:34:41 -0300

    Fix error in filter endpoint - 2

--- app/api/routes/filters/searchProductsByScore/route.js
+++ app/api/routes/filters/searchProductsByScore/route.js
@@ -9,7 +9,7 @@ import {
   User,
 } from "@/db/models/models";
 
-export async function GET() {
+export async function POST() {
   try {
     const data = await Product.findAll({
       include: [
@@ -20,8 +20,8 @@ export async function GET() {
         { model: Categories },
         { model: Tags },
       ],
+      order: [["score", "DESC"]],
     });
-    
     return Response.json(data);
   } catch (error) {
     return Response.json(error.message, { status: 500 });
