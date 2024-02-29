b0579299c3c5ffcefcba5fdf66f09367b5194ba9
AhrensOG <ahrensgabriel09@gmail.com>
2024-02-28 23:25:54 -0300

    Error to add products to cart - 2

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import GlobalContext from "./context/GlobalContext";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/footer/Footer";
 import { Toaster } from "sonner";
-import WhatsAppButton from "@/components/whatsapp/WhatsAppButton";
+// import WhatsAppButton from "@/components/whatsapp/WhatsAppButton";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
           <Navbar />
           <div className="flex-1">{children}</div>
           <Footer />
-          <WhatsAppButton/>
+          {/* <WhatsAppButton/> */}
         </body>
       </GlobalContext>
     </html>
