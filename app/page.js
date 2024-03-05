1096d450769268a09d93f6e61364e86703ffcc0a
AhrensOG <ahrensog@gmail.com>
2024-03-05 15:37:43 -0300

    Add TopCategoriesSection to home page | Add link to redirect to shop page with a category selected

--- app/page.js
+++ app/page.js
@@ -2,11 +2,13 @@
 
 import Hero from "@/components/home/hero/Hero";
 import ProductsSection from "@/components/home/products/ProductsSection";
+import TopCategoriesSection from "@/components/home/topCategories/TopCategoriesSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <TopCategoriesSection/>
       <ProductsSection />
     </main>
   );
