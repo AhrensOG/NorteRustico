445ee6b6e74cb00551d7a9e4af6d7a4866ed15f1
AhrensSG <guillermoahrens@gmail.com>
2024-05-05 22:08:29 -0300

    Change principal Categories Images

--- components/home/topCategories/TopCategoriesSection.jsx
+++ components/home/topCategories/TopCategoriesSection.jsx
@@ -5,8 +5,8 @@ const TopCategoriesSection = () => {
   return (
     <div className='w-full pt-16 flex flex-row justify-center items-center'>
       <div className='flex flex-row flex-wrap justify-center items-center w-full gap-8 max-w-screen-xl'>
-        <TopCategoriesSectionCard title={"Tejido Chaguar"} link={"Chaguar"} img={'https://firebasestorage.googleapis.com/v0/b/norte-rustico.appspot.com/o/TejidoChaguar.webp?alt=media&token=ec50e930-3442-4496-b326-f3c9b9212a4c'}/>
-        <TopCategoriesSectionCard title={"Tejido Lana de Oveja"} link={"Lana de Oveja"} img={'https://firebasestorage.googleapis.com/v0/b/norte-rustico.appspot.com/o/TejidoLanaDeOveja.webp?alt=media&token=c494ad4c-4155-4842-8119-df745e8492de'}/>
+        <TopCategoriesSectionCard title={"Tejido Chaguar"} link={"Chaguar"} img={'https://firebasestorage.googleapis.com/v0/b/norte-rustico.appspot.com/o/BolsosChaguar.jpeg?alt=media&token=f8419f3f-302d-40f8-bc02-8f359dd0d022'}/>
+        <TopCategoriesSectionCard title={"Tejido Lana de Oveja"} link={"Lana de Oveja"} img={'https://firebasestorage.googleapis.com/v0/b/norte-rustico.appspot.com/o/TejidoDeLanaOveja.jpeg?alt=media&token=eb0ae360-335a-4fb9-a49f-ca0356e4ef9a'}/>
         <TopCategoriesSectionCard title={"Carandillo"} link={"Carandillo"} img={'https://firebasestorage.googleapis.com/v0/b/norte-rustico.appspot.com/o/Carandillo.webp?alt=media&token=62345f16-6a41-43cc-ad80-26663d0c00f6'}/>
       </div>
     </div>
